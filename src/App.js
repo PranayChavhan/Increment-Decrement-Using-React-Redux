5d80552376a1c4fd3316e9400f927b85b7ee33ba
PranayChavhan <pranay21chavhan@outlook.com>
2022-01-08 19:03:04 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {incNumber, decNumber} from "./actions/index";
 
 function App() {
+
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Increment/Decrement counter</h1>
+        <h4>using React and Redux</h4>
+
+        <div className="quantity">
+          <a className="quantity_minus" title="Decrement"
+          onClick={()=> dispatch(decNumber())}
+          >
+            <span>-</span>
+          </a>
+          <input
+            type="text"
+            name="quantity"
+            className="quantity_input"
+            value={myState}
+          />
+          <a className="quantity_plus" title="Increment"
+          onClick={()=> dispatch(incNumber())}
+          >
+            <span>+</span>
+          </a>
+        </div>
       </header>
     </div>
   );
